cb53654e9ce184bc25fe6c21147b0af98d6bf641
Robin Suter <suter.robin@gmail.com>
2018-03-13 13:31:06 +0100

    implement leaflet demo in react

--- react-leaflet-demo/src/App.js
+++ react-leaflet-demo/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MyMap from './MyMap.js';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showLayer: false
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({showLayer: event.target.checked});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <MyMap showLayer={this.state.showLayer}/>
+        <div id="control">
+          <input name="layercheckbox" type="checkbox" checked={this.state.showlayer} onChange={this.handleChange} />
+          <label htmlFor="layercheckbox">Show Layer</label>
+        </div>
       </div>
     );
   }
