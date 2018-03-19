6b021c2b945e3bf2cbdb47559816742185d4bc46
Robin Suter <suter.robin@gmail.com>
2018-03-19 10:05:27 +0100

    cleanup

--- vuejs-leaflet-demo/src/main.js
+++ vuejs-leaflet-demo/src/main.js
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import Vue2Leaflet from 'vue2-leaflet';
-
-// Vue.component('v-map', Vue2Leaflet.Map);
-// Vue.component('v-tilelayer', Vue2Leaflet.TileLayer);
-// Vue.component('v-marker', Vue2Leaflet.Marker);
 
 new Vue({
   el: '#app',
